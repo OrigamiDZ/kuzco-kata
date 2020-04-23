feaed0cb79e5919e36f179eb526b1dd10e82c185
aurelia.del.zotto <aurelia96@free.fr>
2020-04-23 10:10:41 +0200

    DataSource, repository start

---
+++ src/test/kotlin/repository/HotelKuzcoRepositoryTest.kt
@@ -0,0 +1,10 @@
+package repository
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HotelKuzcoRepositoryTest {
+
+    @org.junit.jupiter.api.Test
+    fun getRoomsInformations() {
+    }
+}
